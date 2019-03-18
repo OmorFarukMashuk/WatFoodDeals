5439b460ae26bbc59edbdba23b3afa482b8802f7
Mohd Nazim <mohdazfarnazim@v1041-wn-rt-b-249-16.campus-dynamic.uwaterloo.ca>
2019-03-17 20:10:19 -0400

    dropDownAdded

--- ECE_project/HomePage.swift
+++ ECE_project/HomePage.swift
@@ -10,22 +10,33 @@ import UIKit
 
 class HomePage: UIViewController {
    
-   
+    //Actions
+    @IBOutlet var optionButtons: [UIButton]!
+    
+    
     @IBAction func HomeSegueMain(_ sender: Any) {
         self.performSegue(withIdentifier: "HomeSegue3", sender: self)
     }
-    
-    
-    
-    
-
-    
-    
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func handleOptions(_ sender: UIButton) {
+        optionButtons.forEach { (button) in
+            UIView.animate(withDuration: 0.3, animations: {
+                button.isHidden = !button.isHidden
+                self.view.layoutIfNeeded()
+            })
+            
+                    }
+    }
+    
+    
+    @IBAction func itemSelected(_ sender: UIButton) {
+            }
+    
+ 
+    
 }
---
+++ ECE_project/dropDown.swift
@@ -0,0 +1,9 @@
+//
+//  dropDown.swift
+//  ECE_project
+//
+//  Created by Mohd Azfar Nazim on 2019-03-10.
+//  Copyright © 2019 Ibrahim  Khalil. All rights reserved.
+//
+
+import Foundation
