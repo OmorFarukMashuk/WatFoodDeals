8cff71a3616936ee261f9d43f581275d8b241a97
Omor Faruk Mashuk <im.ofarmash@gmail.com>
2019-03-18 02:38:25 -0400

    appdelegate edited

--- ECE_project/AppDelegate.swift
+++ ECE_project/AppDelegate.swift
@@ -18,6 +18,7 @@ UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let a = ""
         return true
     }
 
